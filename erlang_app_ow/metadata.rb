f83a79a76add0d34f35e3126f8805b27fc6f55d4
Eric Cestari <ecestari@mac.com>
2013-09-20 12:40:45 +0200

    forgot to save this one

--- erlang_app_ow/metadata.rb
+++ erlang_app_ow/metadata.rb
@@ -1,1 +1,13 @@
-metadata.rb
+name             "erlang_app_ow"
+maintainer        "Eric Cestari"
+maintainer_email  "ecestari@gmail.com"
+license           "Apache 2.0"
+description       "Deploys an erlang app (rebar required)"
+version           "1.3.3"
+
+
+recipe "erlang_app_ow", "Installs Erlang via native package, source, or Erlang Solutions package"
+
+%w{ ubuntu  }.each do |os|
+  supports os
+end
