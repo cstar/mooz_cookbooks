a0ea69bfe7e9a0d74055ffc3836f1a29ed6e0926
Eric Cestari <ecestari@mac.com>
2013-09-27 15:41:26 +0200

    default => node

--- moozfront/attributes/default.rb
+++ moozfront/attributes/default.rb
@@ -1,3 +1,3 @@
 default[:moozfront][:elasticsearch_elb] = nil
 default[:moozfront][:force_deps] = false 
-default[:deploy][:moozfront][:symlink_before_migrate] = {"erlasticsearch.config" => "erlasticsearch.config", 'boss' => "boss"}
+node[:deploy][:moozfront][:symlink_before_migrate] = {"erlasticsearch.config" => "erlasticsearch.config", 'boss' => "boss"}
