071ef66923b94de8dea1664134c053abea098473
Eric Cestari <ecestari@mac.com>
2013-10-21 16:37:59 +0200

    bulkimporter recipe

---
+++ moozfront/recipes/bulkimporter.rb
@@ -0,0 +1,75 @@
+include_recipe 'deploy'
+
+node[:deploy].each do |application, deploy|
+  next if not node[:opsworks][:instance][:layers].include? application
+
+  opsworks_deploy_user do
+    deploy_data deploy
+  end
+
+  directory "#{deploy[:deploy_to]}/shared" do
+    group deploy[:group]
+    owner deploy[:user]
+    mode 0775
+    action :create
+    recursive true
+  end
+
+
+  # create shared/ directory structure
+  ['log','deps', 'ebin'].each do |dir_name|
+    directory "#{deploy[:deploy_to]}/shared/#{dir_name}" do
+      group deploy[:group]
+      owner deploy[:user]
+      mode 0775
+      action :create
+      recursive true
+    end
+  end
+
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+
+  ['ebin','deps'].each do |dir_name|
+    link "#{deploy[:deploy_to]}/current/#{dir_name}" do
+      target_file "#{deploy[:deploy_to]}/current/#{dir_name}"
+      to "#{deploy[:deploy_to]}/shared/#{dir_name}"
+      group deploy[:group]
+      owner deploy[:user]
+    end
+  end
+
+  template "#{deploy[:deploy_to]}/current/sys.config" do
+    mode 0755
+    source "sys.config.erb"
+    group deploy[:group]
+    owner deploy[:user]
+    variables :app => node[:moozfront]
+  end
+
+
+
+  if node[:moozfront][:force_deps] == true
+    execute "remove deps" do
+      command "rm -Rf *"
+      cwd "#{deploy[:deploy_to]}/shared/deps"
+      user deploy[:user]
+    end
+    execute "fetch deps" do
+      command "./rebar get-deps compile"
+      cwd "#{deploy[:deploy_to]}/current"
+      user deploy[:user]
+    end
+  end
+
+
+  execute "compile app" do
+    command "./rebar compile skip_deps=true"
+    cwd "#{deploy[:deploy_to]}/current"
+    user deploy[:user]
+  end
+
+end
