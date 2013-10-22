b935c2ba610f311527282e639808dcf80ff2296b
Eric Cestari <ecestari@mac.com>
2013-10-22 10:31:17 +0200

    put correct metadata

--- moozfront/metadata.rb
+++ moozfront/metadata.rb
@@ -6,8 +6,10 @@ description       "Deploys an erlang app (rebar required)"
 version           "1.3.3"
 
 
-recipe "mooz", "Installs moozfront"
-recipe "bulkimporter", "Installs bulkimporter"
+recipe "moozfront::default", "Installs moozfront"
+recipe "moozfront::bulkimporter", "Installs bulkimporter"
+
+depends "s3_file"
 
 %w{ ubuntu  }.each do |os|
   supports os
